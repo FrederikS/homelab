160f5547022b3b8e2da00ec885c34f511c7bade9
fdk <fdk@fdk.codes>
2025-04-28 20:41:43 +0200

    added homelab inventory

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec { })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {  
+          packages = with pkgs; [ ansible python311 virtualenv ] ++
+            (with pkgs.python311Packages; [ pip netaddr ipython ]);
+        };
+      });
+    };
+}
