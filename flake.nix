ca0a85f49bc509d63a8086d2697dd5942beff9f1
fdk <fdk@fdk.codes>
2025-08-24 14:00:38 +0200

    added pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -5,25 +5,62 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       overlays = [
         (final: prev: rec { })
       ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit overlays system; };
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {  
-          packages = with pkgs; [ ansible ansible-lint python311 virtualenv fluxcd sops age cloudflared kustomize kubectl-cnpg openssl ] ++
-            (with pkgs.python311Packages; [ pip netaddr ipython ]);
-          # shellHook = ''
-          #   . <(flux completion zsh)
-          # '';
-        };
-      });
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                ansible
+                ansible-lint
+                python311
+                virtualenv
+                fluxcd
+                sops
+                age
+                cloudflared
+                kustomize
+                kubectl-cnpg
+                openssl
+                pre-commit
+                nodejs
+                prettier
+                shellcheck
+              ]
+              ++ (with pkgs.python311Packages; [
+                pip
+                netaddr
+                ipython
+                typos
+              ]);
+            # shellHook = ''
+            #   . <(flux completion zsh)
+            # '';
+          };
+        }
+      );
     };
 }
